cada7681916c8a4ec3e8c8c1d33baa053d21e94e
CnC-RaVaGe <iravage@protonmail.com>
2024-09-02 08:05:00 +0100

    Added toolbar button for Map Lighting properties. Added toolbar button for Map INI Editor. Added toolbar button for Macro Scripts tool.

--- MissionEditor/resource.h
+++ MissionEditor/resource.h
@@ -606,6 +606,7 @@
 #define ID_VIEW_SHOWSMUDGES             40166
 #define ID_VIEW_SHOWBASENODES           40167
 #define ID_VIEW_SHOWCELL                40168
+#define ID_BUTTON40171                  40171
 #define IDS_LINEARVERSION               57604
 #define IDC_TOOLTIPCENTER               65535
 
@@ -614,7 +615,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        302
-#define _APS_NEXT_COMMAND_VALUE         40169
+#define _APS_NEXT_COMMAND_VALUE         40172
 #define _APS_NEXT_CONTROL_VALUE         1464
 #define _APS_NEXT_SYMED_VALUE           111
 #endif
